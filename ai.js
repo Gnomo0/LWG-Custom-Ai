f3d0f2d25ae9e18f40d53e9d700f8922acffa9f0
Gnomo0 <109523522+Gnomo0@users.noreply.github.com>
2023-10-20 10:22:42 -0300

    idk

--- ai.js
+++ ai.js
@@ -1,93 +1,27 @@
 const myPlayerNumber = scope.getMyPlayerNumber();
 
-function sortArrayGroundDistance(Array,X,Y){
-
-    if(!Array  || typeof Array != "Array") return;
-
-    let ArrayCopy = Array.concat();
-
-    ArrayCopy.sort((a,b) =>{
-        let distanceA = scope.getGroundDistance(a.getX(),a.getY(),X,Y);
-        let distanceB = scope.getGroundDistance(b.getX(),b.getY(),X,Y);
-
-        return distanceA - distanceB;
-    });
-
-    return ArrayCopy;
+function getOwnedBuildings(building) {
+  return scope.getBuidlings({ type: building, player: myPlayerNumber });
 }
 
-function filterUnitsInRange(Array,X,Y,Range){
+function getOwnedUnits(){}
 
-    if(!Array || typeof Array != "Array") return;
-
-    let arrayCopy = Array.concat()
-    let newArray = []
-    let distance = 0
-
-    arrayCopy.forEach((item) => {
-
-        distance = scope.getGroundDistance(item.getX(),item.getY(),X,Y)
-
-        if(distance > range) return;
-
-        newArray.push(item);
-
-        return;
-    });
-
-    let sortedNewArray = sortArrayGroundDistance(newArray,X,Y);
-    return sortedNewArray;
+function isProducing(unit) {
+  if (unit.getUnitTypeNameInProductionQueAt(1)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function trainWorkers(){
-    let castles = scope.getBuildings({type:"Castle",player:myPlayerNumber});
-    let workers = scope.getUinits({type:"Worker",player:myPlayerNumber});
-    let workerCost = workers[0].getFieldValue("Cost");
-    let gold = scope.getGold();
+function manageCastles() {
+  let castles = scope.getBuildings("Castle");
 
-    if(gold < workerCost || castles.length * 11 < workers.length) return;
+  if (!castles[0]) return;
 
-    castles.forEach(castle => {
+  castles.forEach((castle) => {
+    if (isProducing(castle)) return;
 
-        if(!castle.getUnitTypeNameInProductionQueAt(1)){
-            scope.order("Train Worker", [castle] , false);
-        }
-
-        gold = scope.getGold();
-
-        if(gold < workerCost) return;
-    });
+    let 
+  });
 }
- 
-function unIdleWorkers(){
-    /*
-
-    TOO LAZY DIDN'T FINISH
-
-    let idleWorkers = scope.getUnits({type:"Worker", player:myPlayerNumber,order:"Stop"});
-    let miningWorkers = scope.getUnits({type:"Worker",player:myPlayerNumber,order:"Mine"});
-    let castles = scope.getBuildings({type:"Castle",player:myPlayerNumber});
-    let mines = scope.getBuildings({type:"GoldMine"})
-    let sortedCastles = [];
-
-    idleWorkers.forEach((worker) =>{
-        sortedCastles = sortArrayGroundDistance(castles,worker.getX,worker.getY);
-
-        sortedCastles.forEach((sortedCastle) =>{
-            let filteredminingWorkers = filterUnitsInRange(miningWorkers,sortedCastle.getX(),sortedCastle.getY())
-
-            if (filteredminingWorkers.length > 12) return;
-
-            scope.order
-        })
-    });
-
-    */
-}
-
-function step(){
-    trainWorkers();
-    unIdleWorkers();
-}
-
-setInterval(step, 1000);
