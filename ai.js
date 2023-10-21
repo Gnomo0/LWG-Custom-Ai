22fab788e252efcb40abb220ac63e203eeb09b94
Gnomo0 <109523522+Gnomo0@users.noreply.github.com>
2023-10-21 17:24:30 -0300

    idk

--- ai.js
+++ ai.js
@@ -10,29 +10,33 @@ function isProducing(unit) {
       }
 }
 
+function trainWorker(castle) {
+      if (isProducing(castle)) return;
+
+      let gold = getGold();
+      if (gold < 50) return;
+
+      scope.order("Train Worker", [castle])
+}
+
 function getGold() {
       return scope.getGold();
 }
 
 function manageCastles() {
-      let castles = scope.getBuildings({type: "Castle", player: myPlayerNumber});
+      let castles = scope.getBuildings({ type: "Castle", player: myPlayerNumber });
 
       if (!castles[0]) return;
 
       castles.forEach((castle) => {
-            if (isProducing(castle)) return;
-
-            let gold = getGold();
-            if(gold < 50) return;
-
-            scope.order("Train Worker",[castle])
+            trainWorker(castle);
       });
 }
 
-function run(){
-      try{
+function run() {
+      try {
             manageCastles();
-      } catch(e){
+      } catch (e) {
             console.error(e)
       }
 }
