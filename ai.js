723dff90390ffd55df9cbe5af916277a0e49754b
Gnomo0 <109523522+Gnomo0@users.noreply.github.com>
2023-10-21 17:17:12 -0300

    added manageCastles?

--- ai.js
+++ ai.js
@@ -1,27 +1,38 @@
 const myPlayerNumber = scope.getMyPlayerNumber();
 
-function getOwnedBuildings(building) {
-  return scope.getBuidlings({ type: building, player: myPlayerNumber });
-}
-
-function getOwnedUnits(){}
+function getOwnedUnits() { }
 
 function isProducing(unit) {
-  if (unit.getUnitTypeNameInProductionQueAt(1)) {
-    return true;
-  } else {
-    return false;
-  }
+      if (unit.getUnitTypeNameInProductionQueAt(1)) {
+            return true;
+      } else {
+            return false;
+      }
+}
+
+function getGold() {
+      return scope.getGold();
 }
 
 function manageCastles() {
-  let castles = scope.getBuildings("Castle");
+      let castles = scope.getBuildings({type: "Castle", player: myPlayerNumber});
 
-  if (!castles[0]) return;
+      if (!castles[0]) return;
 
-  castles.forEach((castle) => {
-    if (isProducing(castle)) return;
+      castles.forEach((castle) => {
+            if (isProducing(castle)) return;
 
-    let 
-  });
+            let gold = getGold();
+            if(gold < 50) return;
+
+            scope.order("Train Worker",[castle])
+      });
+}
+
+function run(){
+      try{
+            manageCastles();
+      } catch(e){
+            console.error(e)
+      }
 }
