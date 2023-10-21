da4b28660781ff19456167e0272ca4f59c3ca990
Gnomo0 <luiz.limauniform@gmail.com>
2023-10-21 19:50:46 -0300

    added mining function

--- ai.js
+++ ai.js
@@ -1,5 +1,11 @@
 const myPlayerNumber = scope.getMyPlayerNumber();
 
+const settings = {
+      "maxWorkerToCastleRatio": 10,
+      "minGoldForWorkerProduction": 50,
+      "maxWorkersOnMine": 6,
+}
+
 function isProducing(unit) {
       if (unit.getUnitTypeNameInProductionQueAt(1)) {
             return true;
@@ -8,22 +14,25 @@ function isProducing(unit) {
       }
 }
 
-function trainWorker(castle) {
-      if (isProducing(castle)) return;
-
-      let gold = getGold();
-      if (gold < 50) return;
-
-      scope.order("Train Worker", [castle])
-}
-
 function getGold() {
       return scope.getGold();
 }
 
+function trainWorker(castle) {
+      if (isProducing(castle)) return;
+
+      let castles = scope.getBuildings({ type: "Castle", player: myPlayerNumber });
+      let workers = scope.getUnits({ type: "Worker", player: myPlayerNumber });
+      if (workers.length / castles.length >= settings.maxWorkerToCastleRatio) return;
+
+      let gold = getGold();
+      if (gold < settings.minGoldForWorkerProduction) return;
+
+      scope.order("Train Worker", [castle])
+}
+
 function manageCastles() {
       let castles = scope.getBuildings({ type: "Castle", player: myPlayerNumber });
-      console.log(castles)
 
       if (!castles[0]) return;
 
@@ -32,11 +41,43 @@ function manageCastles() {
       });
 }
 
+function getOrderedListOfMinesByDistanceFromUnit(unit) {
+      let mines = scope.getBuildings({ type: "Goldmine" });
+      if (!mines[0]) return;
+
+      let newMines = mines.sort(function (a, b) {
+            let distanceA = Math.pow(a.getX() - unit.getX(), 2) + Math.pow(a.getY() - unit.getY(), 2);
+            let distanceB = Math.pow(b.getX() - unit.getX(), 2) + Math.pow(b.getY() - unit.getY(), 2);
+
+            return distanceA - distanceB;
+      });
+
+      return newMines;
+}
+
+function workNearestUnsaturatedMine(workerToStartWorking) {
+      
+      let orderedMines = getOrderedListOfMinesByDistanceFromUnit(workerToStartWorking);
+
+      scope.order("Mine", [workerToStartWorking], {unit: orderedMines[0]});
+}
+
+function manageWorkers() {
+      let workers = scope.getUnits({ type: "Worker", player: myPlayerNumber });
+
+      if (!workers[0]) return;
+
+      workers.forEach(worker => {
+            workNearestUnsaturatedMine(worker)
+      });
+}
+
 function run() {
       try {
             manageCastles();
+            manageWorkers();
       } catch (e) {
-            console.error(e)
+            console.log(e)
       }
 }
 
