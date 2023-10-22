cf8759b6c228f30503097d477ce6f78ed24a9fc9
Gnomo0 <luiz.limauniform@gmail.com>
2023-10-22 00:11:17 -0300

    Improved some things

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # LWG-Custom-Ai
 
+This is my simple ai project
+
 Little War game : https://littlewargame.com/
 
-Ai api documentation : https://web.archive.org/web/20160309173754/http://littlewargame.com/blog/the-littlewargame-ai-api/
+Ai api documentation : https://web.archive.org/web/20160309173754/http://littlewargame.com/blog/the-littlewargame-ai-api/ , http://dev.littlewargame.com/api/ai/documentation.html
--- ai.js
+++ ai.js
@@ -4,6 +4,12 @@ const settings = {
       "maxWorkerToCastleRatio": 10,
       "minGoldForWorkerProduction": 50,
       "maxWorkersOnMine": 6,
+      "mapwidth": scope.getMapWidth(),
+      "mapHeight": scope.getMapHeight(),
+}
+
+var globals = {
+      workers: {},
 }
 
 function isProducing(unit) {
@@ -56,10 +62,18 @@ function getOrderedListOfMinesByDistanceFromUnit(unit) {
 }
 
 function workNearestUnsaturatedMine(workerToStartWorking) {
-      
+
       let orderedMines = getOrderedListOfMinesByDistanceFromUnit(workerToStartWorking);
 
-      scope.order("Mine", [workerToStartWorking], {unit: orderedMines[0]});
+      scope.order("Mine", [workerToStartWorking], { unit: orderedMines[0] });
+}
+
+function handleWorkersByOrder() {
+      if (globals.workers.Stop) {
+            globals.workers.Stop.forEach(StopWorker => {
+                  workNearestUnsaturatedMine(StopWorker);
+            });
+      }
 }
 
 function manageWorkers() {
@@ -68,14 +82,69 @@ function manageWorkers() {
       if (!workers[0]) return;
 
       workers.forEach(worker => {
-            workNearestUnsaturatedMine(worker)
+            let currentOrder = worker.getCurrentOrderName()
+
+            if (!globals.workers[currentOrder]) {
+                  globals.workers[currentOrder] = [];
+            }
+            globals.workers[currentOrder].push(worker)
       });
+
+      handleWorkersByOrder()
+}
+
+function findBuildableSpot(width, centerOffsetX, centerOffsetY) {
+      let coords = {
+            x: 0,
+            y: 0,
+      }
+
+      return coords;
+}
+
+function getOrderedListOfUnitsByDistanceFromCoords(units, coords) {
+      let orderedUnits = units.sort(function(a,b){
+            let distanceA = Math.pow(a.getX() - coords.x,2) + Math.pow(a.getY() - coords.y,2)
+            let distanceB = Math.pow(b.getX() - coords.x,2) + Math.pow(b.getY() - coords.y,2)
+
+            return distanceA - distanceB;
+      });
+
+      return orderedUnits;
+}
+
+function build() {
+
+      if (!globals.workers.Mine) return;
+
+      let supply = {
+            "Current": scope.getCurrentSupply(),
+            "Max": scope.getMaxSupply(),
+            "Remaining": supply.Max - supply.Current,
+      };
+
+      let castles = scope.getBuildings({ type: "Castle", player: myPlayerNumber });
+
+      let productionCapability = castles.length * 2;
+
+
+      //Build House
+
+      if (supply.Remaining - productionCapability <= 0) {
+            let coords = findBuildableSpot(3, 1, 1);
+            let builder = getOrderedListOfUnitsByDistanceFromCoords(globals.workers.Mine, coords)[0]
+
+            scope.order("Build House",[builder],{x:coords.x,y:coords.y})
+      }
+
 }
 
 function run() {
       try {
             manageCastles();
             manageWorkers();
+            build()
+            console.log(globals);
       } catch (e) {
             console.log(e)
       }
